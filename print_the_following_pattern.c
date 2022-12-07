be12ed5f58258ab9e3728a860f9279568b5aad70
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2022-12-07 19:37:16 +0530

    #include<stdio.h> int main(){     int n;     int i=1;     int u=1;     scanf("%d",&n);int t=n;     int y=t;     while (n!=0)     {         while (i<=n)         {             printf("%d",u);             u++;             i++;         }         printf(" ");         i=1;         u=1;         n--;
        }
        
    }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int i=1;
+    int u=1;
+    scanf("%d",&n);int t=n;
+    int y=t;
+    while (n!=0)
+    {
+        while (i<=n)
+        {
+            printf("%d",u);
+            u++;
+            i++;
+        }
+        printf("
+");
+        i=1;
+        u=1;
+        n--;
+
+        
+    }
+    
+}
