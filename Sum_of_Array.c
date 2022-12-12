3e0c3758b8f7c70c39c6aa50772d47c7c6665b3f
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2022-12-12 11:37:06 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int t=n-1;     int ar[t];     int  i=0;     while(i<=t){         scanf("%d",&ar[i]);         i++;     }     i=0;     int max=0;     while(i<=t){         max=max+ar[i];        i++;     }     printf("%d",max);
    }

---
+++ Sum_of_Array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int t=n-1;
+    int ar[t];
+    int  i=0;
+    while(i<=t){
+        scanf("%d",&ar[i]);
+        i++;
+    }
+    i=0;
+    int max=0;
+    while(i<=t){
+        max=max+ar[i];
+       i++;
+    }
+    printf("%d",max);
+    
+}
