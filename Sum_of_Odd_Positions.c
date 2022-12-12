efafef32134a1c89e0116cb71cf9653bf50e4977
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2022-12-12 12:14:01 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int t=n-1;     int ar[t];     int  i=0;     while(i<=t){         scanf("%d",&ar[i]);         i++;     }     i=0;     int max=0;     int sum=0;     while(i<=t){         if(i%2!=0){             max=max+ar[i];         }         else{             sum=sum+ar[i];         }        i++;     }     printf("%d",max);
    }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int t=n-1;
+    int ar[t];
+    int  i=0;
+    while(i<=t){
+        scanf("%d",&ar[i]);
+        i++;
+    }
+    i=0;
+    int max=0;
+    int sum=0;
+    while(i<=t){
+        if(i%2!=0){
+            max=max+ar[i];
+        }
+        else{
+            sum=sum+ar[i];
+        }
+       i++;
+    }
+    printf("%d",max);
+    
+}
+    
