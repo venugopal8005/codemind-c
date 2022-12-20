933158e3ce78d23150b7805acefd321edff26222
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2022-12-20 09:25:36 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c,j;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(a[i]==a[j] && i!=j)             {                 c++;             }         }         if(c==0)         {             printf("%d",a[i]);         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,c,j;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j] && i!=j)
+            {
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            printf("%d",a[i]);
+        }
+    }
+}
