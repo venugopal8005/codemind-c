e741d83f025121f930ad5989f00ecf6f1641d796
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2023-01-03 11:34:37 +0530

    #include <stdio.h> #include <string.h>
    int main()
    {
        char s[200];
        int count = 0, i;
     
        
        scanf("%[^
    ]s", s);
        for (i = 0;s[i] != NULL;i++)
        {
            if (s[i] == ' ')
                count++;    
        }
        printf("%d
    ", count + 1);
    }

---
+++ Count_no._of_words.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[200];
+    int count = 0, i;
+ 
+    
+    scanf("%[^
+]s", s);
+    for (i = 0;s[i] != NULL;i++)
+    {
+        if (s[i] == ' ')
+            count++;    
+    }
+    printf("%d
+", count + 1);
+}
