b2c1cd337cc4121e80eb5fbf05662e079bc3fe43
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2023-05-21 22:24:03 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],f[n],i,j,c,max=0,min;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         f[i]=-1;     }     for(i=0;i<n;i++)     {         c=1;         for(j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 c++;                 f[j]=0;             }         }         if(f[i]!=0)         {             f[i]=c;         }     }     for(i=0;i<n;i++)     {         if(max<f[i])         {             max=f[i];         }     }     for(i=0;i<n;i++)     {         if(f[i]==max)         {             min=a[i];         }     }     for(i=0;i<n;i++)     {         if(f[i]==max)         {             if(min>a[i])             {                 min=a[i];             }         }     }     for(i=0;i<n;i++)     {         if(f[i]==max)         {             if(min==a[i])             {                 printf("%d",a[i]);             }         }     } }

---
+++ Most_Frequent.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],f[n],i,j,c,max=0,min;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        f[i]=-1;
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+                f[j]=0;
+            }
+        }
+        if(f[i]!=0)
+        {
+            f[i]=c;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(max<f[i])
+        {
+            max=f[i];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(f[i]==max)
+        {
+            min=a[i];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(f[i]==max)
+        {
+            if(min>a[i])
+            {
+                min=a[i];
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(f[i]==max)
+        {
+            if(min==a[i])
+            {
+                printf("%d",a[i]);
+            }
+        }
+    }
+}
