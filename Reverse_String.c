7fada6c12f92a6fd35328465524f9908736be36b
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2023-06-05 10:47:55 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[100];     scanf("%[^ ]s",s);     int l=strlen(s),i;     for(i=l-1;i>=0;i--)     {         printf("%c",s[i]);     } }

--- Reverse_String.c
+++ Reverse_String.c
@@ -2,26 +2,12 @@
 #include<string.h>
 int main()
 {
-    char a[100];
+    char s[100];
     scanf("%[^
-]s",a);
-    int i,l=strlen(a),j,k;
+]s",s);
+    int l=strlen(s),i;
     for(i=l-1;i>=0;i--)
     {
-        if(a[i]==' ')
-        {
-            for(j=i+1;a[j]!=' ' && j<l;j++)
-            {
-                printf("%c",a[j]);
-            }
-            printf(" ");
-        }
-        if(i==0)
-        {
-            for(k=0;a[k]!=' ' && k<l;k++)
-            {
-                printf("%c",a[k]);
-            }
-        }
+        printf("%c",s[i]);
     }
 }
