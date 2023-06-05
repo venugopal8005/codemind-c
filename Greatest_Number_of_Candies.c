eb152e330e9a795f568b2fdbc4f1fbd55ef129da
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2023-06-05 11:01:22 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,j,co;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int c;     scanf("%d",&c);     for(i=0;i<n;i++)     {         co=0;         for(j=0;j<n;j++)         {             if(a[i]+c>=a[j])             {                 co++;             }         }         if(co==n)         {             printf("True ");         }         else         {             printf("False ");         }     } }

---
+++ Greatest_Number_of_Candies.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n; 
+    scanf("%d",&n);
+    int a[n],i,j,co;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c;
+    scanf("%d",&c);
+    for(i=0;i<n;i++)
+    {
+        co=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]+c>=a[j])
+            {
+                co++;
+            }
+        }
+        if(co==n)
+        {
+            printf("True ");
+        }
+        else 
+        {
+            printf("False ");
+        }
+    }        
+}
