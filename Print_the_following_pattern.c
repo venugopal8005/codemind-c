1d46634ba3aba44b644fc11fa63033e8f98cc171
venugopal8005 <119839956+venugopal8005@users.noreply.github.com>
2023-06-05 10:33:55 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,j;     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j || j==n-i+1)             {                 printf("%d ",n-i+1);             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 int main()
-{ 
-    int i,n,j; 
-    scanf("%d",&n); 
-    for(i=1;i<=n;i++) 
-    { 
-        for(j=i;j<=n;j++) 
-        { 
-            printf("%d ",j); 
-        } 
+{
+    int n;
+    scanf("%d",&n);
+    int i,j;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==j || j==n-i+1)
+            {
+                printf("%d ",n-i+1);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
         printf("
 ");
     }
